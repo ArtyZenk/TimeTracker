8d842c130e006fb4dbf5b81bd3c3dc62241ade26
Artyom  Guzenko <artyomka13@gmail.com>
2023-05-27 15:25:38 +0300

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Artyom Guzenko on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
